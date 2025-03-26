d492581423e4d4311b07e026f5d2061d1fde754e
adm-boeckli <dboeckli@gmail.com>
2025-03-26 10:49:16 +0100

    add composite embedded primary key example

--- src/main/java/ch/dboeckli/guru/jpa/intro/domain/example/composite/NameId.java
+++ src/main/java/ch/dboeckli/guru/jpa/intro/domain/example/composite/NameId.java
@@ -1,5 +1,6 @@
 package ch.dboeckli.guru.jpa.intro.domain.example.composite;
 
+import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Embeddable
 public class NameId implements Serializable {
     private String firstName;
     private String lastName;
