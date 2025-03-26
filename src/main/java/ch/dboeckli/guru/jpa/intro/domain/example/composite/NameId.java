432670bb0649e5ae7731fd26b07173303e5b55d3
adm-boeckli <dboeckli@gmail.com>
2025-03-26 10:29:02 +0100

    add composite primary key example

---
+++ src/main/java/ch/dboeckli/guru/jpa/intro/domain/example/composite/NameId.java
@@ -0,0 +1,15 @@
+package ch.dboeckli.guru.jpa.intro.domain.example.composite;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class NameId implements Serializable {
+    private String firstName;
+    private String lastName;
+}
